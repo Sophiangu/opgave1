5a750e3b1c2056ccb90088a1d1be38060bdcf80b
Sophiangu <Sophia_nguyen_1@hotmail.com>
2024-06-09 18:18:15 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-];
+module.exports = {
+  // ...
+  "overrides": [
+    {
+      "files": ["*.vue"],
+      "processor": "vue/vue3"
+    }
+  ]
+}
